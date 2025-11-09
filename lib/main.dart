565d27ce7539fee20c8afa48db16ab8ff87a22c2
Ali Hussain <24.alihussainimtiaz.24@gmail.com>
2025-11-09 08:35:21 +0500

    added dependencies shaders & sprung, animation controller

--- lib/main.dart
+++ lib/main.dart
@@ -23,22 +23,47 @@ class DynamicDisplacement extends StatefulWidget {
   State<DynamicDisplacement> createState() => _DynamicDisplacementState();
 }
 
-class _DynamicDisplacementState extends State<DynamicDisplacement> {
+class _DynamicDisplacementState extends State<DynamicDisplacement>
+    with SingleTickerProviderStateMixin {
+  Offset _position = Offset.zero;
+  Offset _delta = Offset.zero;
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _animation = Tween<Offset>(
+      begin: Offset.zero,
+      end: Offset.zero,
+    ).animate(_controller);
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(title: Text('Dynamic Displacement')),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Text(
-          "Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   ",
-          textAlign: TextAlign.start,
-          style: TextStyle(
-            fontSize: 35,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
+      appBar: AppBar(title: Text('Dynamic Displacement'), centerTitle: true),
+      body: GestureDetector(child: textWidget()),
+    );
+  }
+
+  Padding textWidget() {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Text(
+        "Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   Ali Hussain, Flutter Developer, Flutter Dev, Hello world Ali,   ",
+        textAlign: TextAlign.start,
+        style: TextStyle(
+          fontSize: 35,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
